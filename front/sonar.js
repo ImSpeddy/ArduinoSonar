ae90a827b619dad2b72e9f2d2fb9643c5cb47c47
Ethan Mahlstedt <ethan.mahlstedt@hotmail.com>
2025-03-10 21:04:07 -0700

    Started with Dynamic Canvas

--- front/sonar.js
+++ front/sonar.js
@@ -7,7 +7,7 @@ let direction = true
     false | counterclockwise
 */
 
-let range = 0;
+let range = 100;
 
 document.addEventListener("DOMContentLoaded", ()=>{
     ipcRenderer.invoke("getPorts").then((ports)=>{
@@ -34,38 +34,62 @@ let polygon1 = []
 
 function recievedPositionData(angle, distance){
 
-    const coords = calculateCoordinates(angle, distance)
+    const relativeDistance = distance/range
+
+    const coords = calculateCoordinates(angle, relativeDistance)
     polygon1.push(coords)
+
+    render()
 
 }
 
+const canvas = document.getElementById("sonar");
+
+const sonarOrigin = {x: 0.5, y: 1}
+
 function getAbsolutePosition(x, y){
-    const vwWidth = 1600
-    const vwHeight = 900
+
+    
+
+    const vwWidth = canvas.width
+    const vwHeight = canvas.height
     // TODO: Sync VW
 
-    return {x: x/vwWidth, y: y/vwHeight}
+    return {x: x*vwWidth, y: y*vwHeight}
 
 }
 
 function render(){
-    const canvas = document.getElementById("canvas");
+    if (!canvas) return;
+    if (polygon1.length < 2) return;
     if (canvas.getContext) {
         const ctx = canvas.getContext("2d");
-  
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
         ctx.beginPath();
-        ctx.moveTo(800, 900)
+        ctx.fillStyle = "rgb(0, 255, 0)"
+        ctx.moveTo(canvas.width * 0.5, canvas.height)
         polygon1.forEach((e)=>{
-            ctx.lineTo(e.x, e.y)
+            
+            const coords = getAbsolutePosition(e.x, e.y)
+
+            console.log(e)
+            console.log(polygon1)
+            ctx.lineTo(coords.x, coords.y)
+            console.log(e.x)
+            console.log(e.y)
+            console.log(coords.x)
+            console.log(coords.y)
         })
+        ctx.closePath();
         ctx.fill();
     }
 }
 
 function calculateCoordinates(angle, distance){
 
-    let x;
-    let y;
+    let radians = angle + 45 //* (Math.PI / 180);
+    let x = 0.5 + distance * Math.cos(radians);
+    let y = 1 + distance * Math.sin(radians);
 
     return {x: x, y: y}
 
@@ -106,11 +130,15 @@ function handleCommand(command){
             angle = tokenatedCmd[i+1]+tokenatedCmd[i+2]+tokenatedCmd[i+3]
             angle = parseInt(angle)
 
+            console.log(angle)
+
         }else if(e === 'D'){
 
             distance = tokenatedCmd[i+1]+tokenatedCmd[i+2]+tokenatedCmd[i+3]+tokenatedCmd[i+4]+tokenatedCmd[i+5]
             distance = parseInt(distance)
             distance = distance/100
+
+            console.log(distance)
 
         }else if(e === 'L'){
             reachedLimit()
@@ -128,4 +156,8 @@ document.getElementById("dbg_emit").addEventListener('click', ()=>{
 
     handleCommand(commandLine.value)
     commandLine.value = "";
+
+    handleCommand("G090D02500")
+    handleCommand("G180D02500")
+    handleCommand("G270D02500")
 })
